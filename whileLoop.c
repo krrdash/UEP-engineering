5664090584c8da9568db12e989e2891db8d3f91b
Jędrzej Pomianowski <69614@student.ue.poznan.pl>
2023-03-13 12:19:49 +0100

    Another commit with C program

---
+++ whileLoop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+	int upper, lower, step, iterP;
+	upper = 100;
+	lower = 0;
+	step = 10;
+	iterP = 0;
+
+	while(iterP<=) {
+		iterP+=step;
+		printf("Iterator value is now: %d\n", iterP);
+	}
+
+	printf("Operation completed");
+
+}
